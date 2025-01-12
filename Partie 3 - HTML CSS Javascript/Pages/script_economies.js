ed2e3ebf114ad5a30a1b6e7b76763beed8adfeb3
Vladislav-Levovitch-Polytech-Sorbonne <vladislav.balayan@etu.sorbonne-universite.fr>
2025-01-12 08:11:31 +0100

    Partie 3 - Economies Maj v1

---
+++ Partie 3 - HTML CSS Javascript/Pages/script_economies.js
@@ -0,0 +1,14 @@
+document.getElementById('submit-button').addEventListener('click', () => {
+    const startDate = document.getElementById('start-date').value;
+    const endDate = document.getElementById('end-date').value;
+
+    // Vérification des dates
+    if (!startDate || !endDate || endDate < startDate) {
+        alert("Veuillez sélectionner une plage de dates valide.");
+        return;
+    }
+
+    // Mise à jour de l'URL de l'iframe avec les paramètres
+    const iframe = document.getElementById('graph-frame');
+    iframe.src = `http://127.0.0.1:8000/factures/economies/?start_date=${startDate}&end_date=${endDate}`;
+});
